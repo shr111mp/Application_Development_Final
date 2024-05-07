13857c8cd287abde09f9ecc3d3dd21d971e6e39d
shr111mp <castillobryan.bc67@gmail.com>
2024-05-07 20:54:33 +0800

    added translator page

--- app_final/routes/web.php
+++ app_final/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/translate', function () {
+    return view('translate');
+});
+
+Route::get('/quiz', function () {
+    return view('welcome');
+});
