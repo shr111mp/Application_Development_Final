6f4706eef9ca7ae9df70210713abe30c71e2700b
shr111mp <castillobryan.bc67@gmail.com>
2024-05-07 21:02:23 +0800

    Update web.php

--- app_final/routes/web.php
+++ app_final/routes/web.php
@@ -2,14 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+//Root page
 Route::get('/', function () {
     return view('welcome');
 });
 
+//Translate page
 Route::get('/translate', function () {
     return view('translate');
 });
 
+//Quiz page
 Route::get('/quiz', function () {
     return view('welcome');
 });
